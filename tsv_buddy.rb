1ccb0e18ee25b91f29a7e529b6a4502b75d5284b
shihcy <grockgate@gmail.com>
2017-10-01 16:37:40 +0800

    Solved the homework and all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,34 @@
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
+  TAB = "\t".freeze
+  NEWLINE = "\n".freeze
+
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    @data = tsv.split(NEWLINE)
+		data1 = []
+		@data.each {|element| data1 << element.to_s.split(TAB)}
 
+		hash_data_arr = []
+		i = 1
+		(data1.length-1).times {
+			hash_data = Hash.new
+			j = 0
+			data1[0].each { |symbol|
+				hash_data[symbol] = data1[i][j]
+				j+=1
+			}
+			hash_data_arr << hash_data
+			i+=1
+		}
+
+		@data = hash_data_arr
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+  	@data
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,57 @@
 # Module that can be included (mixin) to take and output Yaml data
+require 'yaml'
+
+module YamlBuddy
+  TAB = "\t".freeze
+  NEWLINE = "\n".freeze
+
+  # take_yaml: converts a String with YAML data into @data
+  # parameter: yaml - a String in YAML format
+  def take_yaml(yaml)
+  	@data = (yaml.split("\n   ")).join
+  	@data = @data.split("\n- ")
+  	@data = @data[1..-1]
+
+		#split data
+		a = []
+		@data.each {|term|a << term.split("\n  ")}
+		a[-1][-1] = a[-1][-1].delete(NEWLINE)
+
+		b = []
+		a.each {|term|
+			c = []
+			term.each {|element| c << (element.delete("'")).split(": ")}
+			b << c
+		}
+		split_data = b
+
+		#hash key
+		key_arr = []
+		(split_data[0].length).times {|i| key_arr << split_data[0][i][0]}
+
+		#hash value
+		value_arr = []
+		(split_data).each {|term|
+			temp = []
+			term.each {|element| temp << element[1]}
+			value_arr << temp
+		}
+
+		#to tsv
+		key_str = key_arr.join(TAB)
+
+		value_str = ""
+		temp_arr = []
+		value_arr.each {|term| temp_arr << term.join(TAB)}
+		value_str = temp_arr.join(NEWLINE)
+	  
+	  @data = ""
+	  @data = key_str + NEWLINE + value_str + NEWLINE
+  end
+
+  # to_yaml: converts @data into yaml string
+  # returns: String in YAML format
+  def to_yaml
+    @data.to_yaml
+  end
+end
